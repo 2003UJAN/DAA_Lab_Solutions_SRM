c8fa2404177660d8db293bb80779cf78007fd300
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2024-04-20 23:35:54 +0530

    Update Linear Search.c

--- 2/Linear Search.c
+++ 2/Linear Search.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n, x;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &x);
+
+    int result = linearSearch(arr, n, x);
+    if (result == -1) {
+        printf("Element is not present in array.\n");
+    } else {
+        printf("Element is present at index %d.\n", result);
+    }
+    return 0;
+}
 

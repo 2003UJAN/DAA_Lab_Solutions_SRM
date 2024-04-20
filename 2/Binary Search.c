b9d21abd4802b4b089aa73a29cc83ddec3d36f9f
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2024-04-20 23:35:05 +0530

    Update Binary Search.c

--- 2/Binary Search.c
+++ 2/Binary Search.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int x) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == x)
+            return mid;
+
+        // If x greater, ignore left half
+        if (arr[mid] < x)
+            l = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            r = mid - 1;
+    }
+
+    // if we reach here, then element was not present
+    return -1;
+}
+
+int main() {
+    int n, x;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &x);
+
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1) ? printf("Element is not present in array.\n")
+                   : printf("Element is present at index %d.\n", result);
+    return 0;
+}
 
